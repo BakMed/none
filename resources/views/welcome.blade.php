64c0c48d3d7aeba53eb0b3a307bab9ad9dbc7d08
EL BAKHIRI <bakme1404@gmail.com>
2023-11-19 23:17:05 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Document</title>
+</head>
+<body>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+        }
+        ::-webkit-scrollbar{
+            width: 5px;
+        }
+        ::-webkit-scrollbar-track{
+            background: #13254c;
+        }
+        ::-webkit-scrollbar-thumb{
+            background: #061128;
+        }
+    </style>
+</body style="background: 05113b;">
+<div>
+    <div class="container-fluid m-0 d-flex p-2">
+        <div class="pl-2" style="width: 40px;heigt: 50px;font-size: 180%;">
+        <i class="fa fa-angle-double-left text-white mt-2"></i>
+        </div>
+        <div style="width: 50px;height: 50px;">
+        <img src="https://cdn.dribbble.com/users/722835/screenshots/4082720/bot_icon.gif" width="100%" height="100%" style="border-radius: 50px;">
+        </div>
+    </div>
+</div>
+</html>
